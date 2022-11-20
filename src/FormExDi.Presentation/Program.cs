72a3ea827b14a97d0084356dea33a7743b00b306
GuilhermeBley <guilhermecbley@gmail.com>
2022-11-20 19:51:23 -0300

    Implement builder scraper #4

--- src/FormExDi.Presentation/Program.cs
+++ src/FormExDi.Presentation/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using BlScraper.DependencyInjection.Extension.Builder;
+
 namespace FormExDi.Presentation;
 
 static class Program
@@ -8,9 +13,38 @@ static class Program
     [STAThread]
     static void Main()
     {
-        // To customize application configuration such as set high DPI settings or default font,
-        // see https://aka.ms/applicationconfiguration.
-        ApplicationConfiguration.Initialize();
-        System.Windows.Forms.Application.Run(new RunScrapGUI(null!));
-    }    
+        System.Windows.Forms.Application.SetHighDpiMode(HighDpiMode.SystemAware);
+        System.Windows.Forms.Application.EnableVisualStyles();
+        System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+
+        var host = Host.CreateDefaultBuilder()
+             .ConfigureAppConfiguration((context, builder) =>
+             {
+                 // Add other configuration files...
+                 builder.AddJsonFile("appsettings.json", optional:false);
+
+             })
+             .ConfigureServices((context, services) =>
+             {
+                 ConfigureServices(context.Configuration, services);
+                 
+             })
+             .ConfigureLogging(logging =>
+             {
+                 // Add other loggers...
+             })
+             .Build();
+
+        var services = host.Services;
+        var mainForm = services.GetRequiredService<RunScrapGUI>();
+        System.Windows.Forms.Application.Run(mainForm);
+    }
+
+    private static void ConfigureServices(IConfiguration configuration, IServiceCollection services)
+    {
+        services
+            .AddSingleton<RunScrapGUI>()
+            .AddScraperBuilder(
+                (builder)=>builder.AddAssembly(typeof(Scrap.Quest.PiedadeMultas.PiedadeMultaQuest).Assembly));
+    }
 }
