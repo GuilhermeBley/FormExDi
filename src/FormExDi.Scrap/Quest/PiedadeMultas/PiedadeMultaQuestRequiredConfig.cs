15f2c254be1102188ce0ffbd0bc7ff17e4aa63a6
GuilhermeBley <guilhermecbley@gmail.com>
2022-11-22 12:45:19 -0300

    Create args #6 - apply to config

--- src/FormExDi.Scrap/Quest/PiedadeMultas/PiedadeMultaQuestRequiredConfig.cs
+++ src/FormExDi.Scrap/Quest/PiedadeMultas/PiedadeMultaQuestRequiredConfig.cs
@@ -1,6 +1,7 @@
 ﻿using BlScraper.DependencyInjection.ConfigureModel;
 using FormExDi.Application.Model;
 using FormExDi.Application.Repository;
+using FormExDi.Scrap.Args;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,13 +12,15 @@ namespace FormExDi.Scrap.Quest.PiedadeMultas;
 
 public class PiedadeMultaQuestRequiredConfig : RequiredConfigure<PiedadeMultaQuest, VehicleModel>
 {
-    public override int initialQuantity => throw new NotImplementedException();
+    public override int initialQuantity => _initArgs.QuantityToRunQuest;
 
     private readonly IVehicleRepository _vehicleRepository;
+    private readonly IInitArgs _initArgs;
 
-    public PiedadeMultaQuestRequiredConfig(IVehicleRepository vehicleRepository)
+    public PiedadeMultaQuestRequiredConfig(IVehicleRepository vehicleRepository, IInitArgs initArgs)
     {
         _vehicleRepository = vehicleRepository;
+        _initArgs = initArgs;
     }
 
     public override Task<IEnumerable<VehicleModel>> GetData()
