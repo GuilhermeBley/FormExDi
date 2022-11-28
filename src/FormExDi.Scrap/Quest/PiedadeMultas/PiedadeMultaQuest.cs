35a9c9e19e6a4763d8a4dd211f275944a4fc8181
GuilhermeBley <guilhermecbley@gmail.com>
2022-11-28 16:30:30 -0300

    Create scraper #5 Di

--- src/FormExDi.Scrap/Quest/PiedadeMultas/PiedadeMultaQuest.cs
+++ src/FormExDi.Scrap/Quest/PiedadeMultas/PiedadeMultaQuest.cs
@@ -1,6 +1,7 @@
 ﻿using BlScraper.DependencyInjection.ConfigureModel;
 using BlScraper.Model;
 using FormExDi.Application.Model;
+using FormExDi.Application.Services.Interface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +12,27 @@ namespace FormExDi.Scrap.Quest.PiedadeMultas;
 
 public class PiedadeMultaQuest : Quest<VehicleModel>
 {
+    private readonly IPiedadeMultaQuery _piedadeMultaQuery;
+    private readonly IInfracaoService _infracaoService;
+
+    public PiedadeMultaQuest(IPiedadeMultaQuery piedadeMultaQuery, IInfracaoService infracaoService)
+    {
+        _piedadeMultaQuery = piedadeMultaQuery;
+        _infracaoService = infracaoService;
+    }
+
     public override QuestResult Execute(VehicleModel data, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        foreach (var multa in _piedadeMultaQuery.GetInfracoesAsync(
+            data.Plate, data.Renavam).GetAwaiter().GetResult())
+        {
+            if (multa.DtInfracao.Year < 2016) // Business rule
+                continue;
+
+            _infracaoService.AddAsync(multa).GetAwaiter().GetResult();
+        }
+
+        return QuestResult.Ok();
     }
 }
 
