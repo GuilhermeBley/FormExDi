68a860dc1ea91ba3f1a396a605e3faaacbd18edd
GuilhermeBley <guilhermecbley@gmail.com>
2022-11-29 09:17:13 -0300

    Create scraper #5 - search

--- src/FormExDi.Scrap/Quest/PiedadeMultas/PiedadeMultaQuery.cs
+++ src/FormExDi.Scrap/Quest/PiedadeMultas/PiedadeMultaQuery.cs
@@ -12,7 +12,7 @@ public interface IPiedadeMultaQuery
 internal class PiedadeMultaQuery : IPiedadeMultaQuery
 {
     private const string url = "http://187.111.170.202:8081/";
-    private const string urlMultas = "http://187.111.170.202:8081/Home/_Resultado?Placa={placaL}-{placaR}&Renavam={renavam}";
+    private const string urlMultas = "http://187.111.170.202:8081/Home/_Resultado?Placa={placa}&Renavam={renavam}";
     private readonly HttpClient _client;
     private readonly HtmlDocument _document;
 
@@ -40,14 +40,20 @@ internal class PiedadeMultaQuery : IPiedadeMultaQuery
         string placaL = string.Concat(placa.Take(4));
         string placaR = string.Concat(placa.Skip(3));
         using (var response = 
-            await _client.GetAsync(urlMultas.Replace("placaL", placaL).Replace("placaR", placaR).Replace("{renavam}", renavam)))
+            await _client.GetAsync(urlMultas.Replace("{placa}", placa).Replace("{renavam}", renavam)))
         {
-            var txt = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+                throw new HttpRequestException();
 
-            if (txt.ToUpper().Contains("NÃO FORAM ENCONTRADAS INFRAÇÕES"))
-                return Enumerable.Empty<InfracaoModel>();
-
-            throw new NotImplementedException();
+            _document.LoadHtml(await response.Content.ReadAsStringAsync());
         }
+        
+        if (_document.DocumentNode.SelectSingleNode($"//*[contains(@text, '{renavam.TrimStart('0')}')]") is null)
+            throw new HttpRequestException("Placa not found in HTML.");
+
+        if (_document.DocumentNode.SelectSingleNode($"Não foram encontradas infrações") is not null)
+            return Enumerable.Empty<InfracaoModel>();
+
+        throw new NotImplementedException();
     }
 }
