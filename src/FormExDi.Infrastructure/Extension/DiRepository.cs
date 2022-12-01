a5dcaf65a50f7c8b4b763aeb164dd7c1ff3ee6cf
GuilhermeBley <guilhermecbley@gmail.com>
2022-12-01 11:43:46 -0300

    Create presentation #3 - add automapper and services

--- src/FormExDi.Infrastructure/Extension/DiRepository.cs
+++ src/FormExDi.Infrastructure/Extension/DiRepository.cs
@@ -1,3 +1,5 @@
+using FormExDi.Application.Services.Implementation;
+using FormExDi.Application.Services.Interface;
 using FormExDi.Application.UoW;
 using FormExDi.Infrastructure.Repository;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,6 +15,14 @@ public static class DiRepository
             .AddScoped<IDbSession>(x => x.GetRequiredService<MySqlDbSession>())
             .AddScoped<IUnitOfWork>(x => x.GetRequiredService<MySqlDbSession>())
             .AddScoped(typeof(IVehicleRepository), typeof(VehicleRepository))
-            .AddScoped(typeof(IInfracaoRepository), typeof(InfracaoRepository));
+            .AddScoped(typeof(IInfracaoRepository), typeof(InfracaoRepository))
+            .AddAutoMapper(typeof(Mappings.InfrastructureProfile));
+    }
+
+    public static IServiceCollection AddServices(this IServiceCollection serviceCollection)
+    {
+        return serviceCollection
+            .AddScoped<IVehicleService, VehicleService>()
+            .AddScoped<IInfracaoService, InfracaoService>();
     }
 }
